8e7dee41d1550657f8b8c10471ff8057ccf1ba4e
deacon94 <mrich1834@gmail.com>
2021-04-08 20:52:07 -0400

    Base code for a couple of test cases

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from unittest import main
 import os
 
 os.system('cls')
-print('\nStarting Time Calculator...\n')
-print(add_time("11:06 PM", "2:02"))
 
 print('\nExecuting Unit Tests...')
 # Run unit tests automatically
 main(module='test_module', exit=False)
+
+print('\nStarting Time Calculator...\n')
+print(add_time("11:06 PM", "2:02"))
+print(add_time("3:30 PM", "2:12"))
--- time_calculator.py
+++ time_calculator.py
@@ -1,18 +1,43 @@
 def add_time(start, duration):
+    
+    # Initialize
+    new_hrs = 0
+    new_mins = 0
 
-# actual = add_time("3:30 PM", "2:12")
-        #expected = "5:42 PM"
-    # Determine starting hours and minutes
-    print(start.find(':'))
-    print(start[0:2])
+    # Determine starting period, hours and minutes (convert to 24 hour set)
+    start_period = start[len(start)-2:]
 
-    start_hrs = int(start[0:start.find(':')])
-    start_secs = int(start[start.find(':')-1:])
+    if start_period == 'PM':
+        start_hrs = int(start[0:start.find(':')]) + 12
+    else:
+        start_hrs = int(start[0:start.find(':')])
+    
+    start_mins = int(start[start.find(':')+1:start.find(':')+3])
 
-    print(start_hrs)
-    print(start_secs)
+    # Determine duration hours and minutes
+    duration_hrs = int(duration[0:duration.find(':')])
+    duration_mins = int(duration[duration.find(':')+1:duration.find(':')+3])
 
-    # temp return
-    new_time = start
+    # Compute new minutes
+    new_mins = start_mins + duration_mins
+
+    if new_mins >= 60:
+        new_hrs = round(new_mins / 60)
+        new_mins = new_mins % 60
+
+    # Compute new hours, period 
+    new_hrs = new_hrs + start_hrs + duration_hrs
+
+    if new_hrs >= 24:
+        new_hrs = new_hrs - 24
+
+    if new_hrs < 12:
+        new_period = 'AM'
+    else:
+        new_period = 'PM'
+        new_hrs = new_hrs - 12
+
+    # Format new time
+    new_time = str(new_hrs) + ':' + str(new_mins).rjust(2, '0') + ' ' + new_period
 
     return new_time
